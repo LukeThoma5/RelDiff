662d167df55a6b79700fe05660a5d951a9f54c56
LukeThoma5 <luke.4man@btinternet.com>
2020-05-08 17:10:01 +0100

    Parse into new structure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
-use git2::Repository;
-use anyhow::{Context, Result};
+use git2::{Repository, Oid, Commit};
+use anyhow::{Context, Result, anyhow};
+use lazy_static::lazy_static;
+use regex::CaptureMatches;
 
 mod git;
 
@@ -24,6 +26,56 @@ pub struct CliOptions {
 
 }
 
+#[derive(Debug, Clone)]
+pub enum ReleaseIdentifier
+{
+    RRQ(u32),
+    TargetProgress(u32)
+}
+
+#[derive(Debug, Clone)]
+pub struct ReleaseItem
+{
+    pub sha1: Oid,
+    pub commit_summary: String,
+    pub ids: Vec<ReleaseIdentifier>
+}
+
+impl ReleaseItem {
+    pub fn new(commit: Commit<'_>) -> Result<Self> {
+
+        let mut r = Self {
+            sha1: commit.id(),
+            commit_summary: commit.message()
+                .ok_or(anyhow!("Failed to get commit message for {}", commit.id()))?
+                .to_owned(),
+            ids: Vec::new()
+        };
+
+
+        use regex::Regex;
+
+        fn extract_matches<'a>(captures: CaptureMatches<'a, 'a>, ctor: impl Fn(u32) -> ReleaseIdentifier + 'a) -> impl Iterator<Item = ReleaseIdentifier> + 'a {
+            captures
+                .filter_map(|i| i.get(2))
+                .filter_map(|i| i.as_str().parse().ok())
+                .map(ctor)
+        };
+
+        lazy_static! {
+            static ref IDREGEX: Regex = Regex::new(r"(id|ID):(\d+)").unwrap();
+            static ref RRQREGEX: Regex = Regex::new(r"(rrq|RRQ):(\d+)").unwrap();
+
+        }
+
+        r.ids = extract_matches(IDREGEX.captures_iter(&r.commit_summary), |i| ReleaseIdentifier::TargetProgress(i))
+            .chain(extract_matches(RRQREGEX.captures_iter(&r.commit_summary), |i| ReleaseIdentifier::RRQ(i)))
+            .collect::<Vec<ReleaseIdentifier>>();
+
+        Ok(r)
+    }
+}
+
 
 fn main() -> Result<()> {
     let opt = CliOptions::from_args();
@@ -32,11 +84,19 @@ fn main() -> Result<()> {
         format!("failed to open repository: {:?}", opt.repo)
     })?;
 
-    let commits = git::get_commits(&repo, &opt).with_context(
+    let commits: Vec<Commit> = git::get_commits(&repo, &opt).with_context(
         || "Failed to find commit difference between branches",
     )?;
 
-    println!("Done {:?}", commits);
+    dbg!("Commits {:?}", &commits);
+
+    let items = commits.into_iter()
+        .map(ReleaseItem::new)
+        .collect::<Result<Vec<_>>>()
+        .context("Failed to convert commits")?;
+
+
+    dbg!("items {:?}", &items);
 
     Ok(())
 }
